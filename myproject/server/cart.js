630c77ef987fe77793487588f64b551173ccb392
Dmitriy Serov <dserov@gmail.com>
2021-02-13 18:08:35 +0300

    lesson7 добавлено логирование событий

--- myproject/server/cart.js
+++ myproject/server/cart.js
@@ -1,12 +1,16 @@
+const logger = require('./logger');
+
 let add = (cart, req) => {
     cart.contents.push(req.body);
     recalcAmount(cart);
+    logger.log('add', req.body.product_name);
     return JSON.stringify(cart, null, 4);
 };
 let change = (cart, req) => {
     let find = cart.contents.find(el => el.id_product === +req.params.id);
     find.quantity += req.body.quantity;
     recalcAmount(cart);
+    logger.log(`increment quantity to ${find.quantity}`, find.product_name);
     return JSON.stringify(cart, null, 4);
 };
 let remove = (cart, req) => {
@@ -16,6 +20,9 @@ let remove = (cart, req) => {
         if (find.quantity === 0) {
             let idx = cart.contents.indexOf(find);
             cart.contents.splice(idx, 1);
+            logger.log(`delete`, find.product_name);
+        } else {
+            logger.log(`decrement quantity to ${find.quantity}`, find.product_name);
         }
     }
     recalcAmount(cart);
